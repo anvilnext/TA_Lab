eb36c8fb97697135f85a81086c7a3dba5306b2f6
Алексей Череватенко <chereva@ukr.net>
2020-02-18 18:27:44 +0200

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TA_Lab
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
